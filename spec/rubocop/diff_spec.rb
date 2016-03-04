01983060d1a10cd4a860aeda837ed44870f67409
Richard McGain <richard.mcgain@shopify.com>
2016-03-04 11:44:01 +0000

    bundle gem rubocop-diff

---
+++ spec/rubocop/diff_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Rubocop::Diff do
+  it 'has a version number' do
+    expect(Rubocop::Diff::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
