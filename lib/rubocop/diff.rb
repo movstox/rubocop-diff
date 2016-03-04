451363a8fd66ce167f73087d8e6a51932881641c
Richard McGain <richard.mcgain@shopify.com>
2016-03-04 17:36:55 +0000

    integrate with rubocop, for now, just print file_finished on that hook

--- lib/rubocop/diff.rb
+++ lib/rubocop/diff.rb
@@ -1,7 +1,14 @@
-require "rubocop/diff/version"
+$LOAD_PATH.unshift File.expand_path('../../', __FILE__)
+require 'rubocop/diff/version'
+require 'rubocop/diff/formatter'
+require 'rubocop'
 
-module Rubocop
-  module Diff
-    # Your code goes here...
+module RuboCop
+  class Diff
+    def run
+      RuboCop::CLI.new.run([
+      '--format', 'RuboCop::Diff::Formatter'
+      ])
+    end
   end
 end
