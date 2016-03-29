e71ce53ad9218df1cc94187e79c3cc1cf56def8f
Richard McGain <richard.mcgain@shopify.com>
2016-03-29 22:40:48 +0100

    pass along other arguments

--- spec/rubocop/diff_spec.rb
+++ spec/rubocop/diff_spec.rb
@@ -5,12 +5,23 @@ describe Rubocop::Diff do
     expect(Rubocop::Diff::VERSION).not_to be nil
   end
 
-  it 'passes command line options to rubocop' do
+  it 'always passes a formatter' do
     cli = object_double(RuboCop::CLI.new)
     expect(RuboCop::CLI).to receive(:new).and_return(cli)
     expect(cli).to receive(:run).with(
       " --format RuboCop::Diff::Formatter"
     )
     RuboCop::Diff.new.run("")
+  end
+
+  it 'passes along other arguments' do
+    cli = object_double(RuboCop::CLI.new)
+    expect(RuboCop::CLI).to receive(:new).and_return(cli)
+    expect(cli).to receive(:run).with([
+      " --debug",
+      " --format RuboCop::Diff::Formatter"
+    ].join('')
+    )
+    RuboCop::Diff.new.run(" --debug")
   end
 end
